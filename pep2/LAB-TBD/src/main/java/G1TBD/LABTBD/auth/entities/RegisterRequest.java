ecf7716a0f7ec8680af86dd6e6f7886f62d6dd53
Nicolas Gajardo <nicolasgajardo11@gmail.com>
2025-05-09 12:41:47 -0400

    Subida de proyecto con jwt

---
+++ pep2/LAB-TBD/src/main/java/G1TBD/LABTBD/auth/entities/RegisterRequest.java
@@ -0,0 +1,31 @@
+package G1TBD.LABTBD.auth.entities;
+
+import G1TBD.LABTBD.data.point.PointEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    private String rut;
+    private String email;
+    private String name;
+    private String lastName;
+    private Date birthDate;
+    private String sex;
+    private String password;
+    private String role;
+    private boolean availability;
+    private LocationRequest location;
+
+}
---
+++ pep2/LAB-TBD/src/test/java/G1TBD/LABTBD/LabTbdApplicationTests.java
@@ -0,0 +1,13 @@
+package G1TBD.LABTBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabTbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
