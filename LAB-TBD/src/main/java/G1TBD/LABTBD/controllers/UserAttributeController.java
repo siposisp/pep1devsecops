d4b1b3fbc78098dab12620e44e4ecdb43ec031af
Nicolas Gajardo <nicolasgajardo11@gmail.com>
2025-05-13 01:14:08 -0400

    Update UserAttributeController.java

--- LAB-TBD/src/main/java/G1TBD/LABTBD/controllers/UserAttributeController.java
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/controllers/UserAttributeController.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api/user-attribute")
-@CrossOrigin(origins = "http://localhost:8097/api/user-attribute")
+@CrossOrigin(origins = "http://localhost:8090/api/user-attribute")
 public class UserAttributeController {
 
     private final UserAttributeService userAttributeService;
